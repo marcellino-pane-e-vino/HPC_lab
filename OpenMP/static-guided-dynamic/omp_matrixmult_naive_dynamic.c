8e56da49cac984bcd5f965669f152b661c71714e
Wassim Fatnassi <62265879+justwsx@users.noreply.github.com>
2026-03-30 12:05:09 +0200

    Remove SIMD directive from matrix multiplication
    Removed SIMD vectorization directive from matrix multiplication loop.

--- OpenMP/static-guided-dynamic/omp_matrixmult_naive_dynamic.c
+++ OpenMP/static-guided-dynamic/omp_matrixmult_naive_dynamic.c
@@ -58,9 +58,6 @@ int main(int argc, char **argv) {
   #pragma omp parallel for schedule(static)
   for (int i = 0; i < n; ++i) {
      for (int k = 0; k < n; k++) {
-        
-        // We force SIMD vectorization inside each core
-        //#pragma omp simd
         for (int j = 0; j < n; ++j) {
            c[i][j] += a[i][k] * b[k][j];
         }
