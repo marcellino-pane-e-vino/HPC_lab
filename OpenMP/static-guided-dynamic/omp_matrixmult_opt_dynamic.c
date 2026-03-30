7a508e87472c90197ae8ef263d7812969f5e84ce
Wassim Fatnassi <62265879+justwsx@users.noreply.github.com>
2026-03-30 12:06:56 +0200

    Remove commented SIMD reduction directive
    Removed commented-out OpenMP SIMD reduction directive for clarity.

--- OpenMP/static-guided-dynamic/omp_matrixmult_opt_dynamic.c
+++ OpenMP/static-guided-dynamic/omp_matrixmult_opt_dynamic.c
@@ -58,7 +58,6 @@ int main(int argc, char **argv) {
                         
                         // A and B_T read linearly(k increases)
                         // Simple "scalar product": icx vectorize it perfectly
-                        //#pragma omp simd reduction(+:sum)
                         for (int k = kk; k < k_end; k++) {
                             sum += a[i * N + k] * b_t[j * N + k];
                         }
