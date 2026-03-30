2f3a7b69f1c166b9e6631a5eb86c5586daff22a6
Luca1702 <80057249+Luca1702@users.noreply.github.com>
2026-03-30 17:37:18 +0200

    Add files via upload

--- Sequential/Sequential_Cleaned/matrixmult_naive.c
+++ Sequential/Sequential_Cleaned/matrixmult_naive.c
@@ -1,70 +1,81 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-
 #include <time.h>
 
 
 int main(int argc,char **argv) {
-   if (argc < 2) {
-        fprintf(stderr, "Error: missing n argument.\n");
-        fprintf(stderr, "Usage: %s <n>\n", argv[0]);
-        return 1;
-    }
 
-    int n = atoi(argv[1]);
-    if (n <= 0) {
-        fprintf(stderr, "Error: You forgot to provide n!.\n");
-        return 1;
-    }
-
-  int i, j, k;
-
-  double ( *a )[n] = malloc(sizeof(double[n][n]));
-  double ( *b )[n] = malloc(sizeof(double[n][n]));
-  double ( *c )[n] = malloc(sizeof(double[n][n]));
-
-  // initialization
-  for (i=0; i<n; i++)
-     for (j=0; j<n; j++) {
-        a[i][j] = 2.0;
-        b[i][j] = 3.0;
-        c[i][j] = 0.0;
-     }
-
+   // Timer for Total Execution Time
    printf("Starting the computation...\n");
-  clock_t start = clock();
-  // computation
-  // note: loop order i-k-j instead of the standard i-j-k --> optimization technique: linear access memory & efficient cache use
-  for (i=0; i<n; ++i)
-     for (k=0; k<n; k++)
-        for (j=0; j<n; ++j)
-           c[i][j] += a[i][k]*b[k][j];
+   clock_t start_total = clock();
 
-   clock_t end = clock();
-
-
-   double duration = (double)(end - start) / CLOCKS_PER_SEC;
-   printf("Execution Time: %.4f seconds\n", duration);
-
-  FILE *f = fopen("mat-res.txt", "w");
-  if (!f) {
-     perror("fopen");
+   // Added for better parameter management
+   if (argc < 2) {
+      fprintf(stderr, "Error: missing n argument.\n");
       return 1;
-  }
+   }
 
-  fprintf(f, "%d\n\n", n);  
-  for (int i = 0; i < 1000; i++) {
-     for (int j = 0; j < 1000; j++) {
-        fprintf(f, "%.0f ", c[i][j]);
-     }
-     fprintf(f, "\n");
-  }
+   int n = atoi(argv[1]);
+   if (n <= 0) {
+      fprintf(stderr, "Error: provided an invalid n!.\n");
+      return 1;
+   }
 
-  fclose(f);
+   int i, j, k;
 
-  free(a);
-  free(b);
-  free(c);
-  return 0;
+   // Memory allocation 
+   double ( *a )[n] = malloc(sizeof(double[n][n]));
+   double ( *b )[n] = malloc(sizeof(double[n][n]));
+   double ( *c )[n] = malloc(sizeof(double[n][n]));
+
+   // Initialization
+   for (i=0; i<n; i++)
+      for (j=0; j<n; j++) {
+         a[i][j] = 2.0;
+         b[i][j] = 3.0;
+         c[i][j] = 0.0;
+      }
+
+
+   // Timer for critical section: hotspot identification
+   printf("Starting the critical section...\n");
+   clock_t start_critical = clock();
+
+   // Matrix multiplication through i-k-j order.
+   for (i=0; i<n; ++i)
+      for (k=0; k<n; k++)
+         for (j=0; j<n; ++j)
+            c[i][j] += a[i][k]*b[k][j];
+
+   clock_t end_critical = clock();
+   double duration = (double)(end_critical - start_critical) / CLOCKS_PER_SEC;
+   printf("Execution Time for the Critical Section: %.4f seconds\n", duration);
+
+   FILE *f = fopen("mat-res.txt", "w");
+   if (!f) {
+      perror("fopen");
+         return 1;
+   }
+
+   fprintf(f, "%d\n\n", n);  
+   for (int i = 0; i < 1000; i++) {
+      for (int j = 0; j < 1000; j++) {
+         fprintf(f, "%.0f ", c[i][j]);
+      }
+      fprintf(f, "\n");
+   }
+
+   fclose(f);
+
+   // Memory deallocation
+   free(a);
+   free(b);
+   free(c);
+   
+   clock_t end_total = clock();
+   double total_time = (double)(end_total - start_total) / CLOCKS_PER_SEC;
+   printf("Total Execution Time: %.4f seconds\n", total_time);
+
+   return 0;
 }
